4a505298e966cb045152335dd2f171022d3de291
kyle Dornan <kyledornan26@gmail.com>
2020-05-28 16:42:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ end
 puts "HAPPY NEW YEAR!"
 end
 
+def countdown(number)
+  number=10
+  while number > 0 
+  sleep(1)
+  puts "#{number} SECOND(S)!"
+  number -= 1
+end 
+puts "HAPPY NEW YEAR!"
+end
